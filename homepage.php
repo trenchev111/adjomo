446e950792f7e04f90dc8af6c8772805ac0a1f28
Trenche <trenche.angov@gmail.com>
2017-02-20 19:44:02 +0100

    last commit by trenche

--- homepage.php
+++ homepage.php
@@ -34,9 +34,31 @@
 			<div class="row first-column">
 				<h2>100%</h2>
 				<h1 class="col-md-12">Monetize your Mobile <span>Traffic</span></h1>
-				<div class="col-md-4 offset-md-4 pull-right">
+				<p class="col-md-5 offset-md-4 pull-right">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia ducimus veniam earum! Architecto omnis ex nobis nemo enim culpa, 
 					natus deleniti assumenda accusantium inventore laboriosam soluta perferendis, corporis facilis sunt?
+				</p>
+			</div>
+
+			<div class="row second-column">
+				<div class="action-text">
+					<h1>Easy to use platform</h1>
+					<p class="col-md-5 offset-md-4 pull-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ab commodi ipsum enim odio debitis labore,
+					 quibusdam autem, eius placeat numquam corporis id cumque repellendus illum ullam, asperiores molestiae eos!</p>
+				</div>
+				<img src="assets/img/mac.png" alt="mac_right" class="mac-img-right pull-right">
+				<img src="assets/img/iphone-right.png" alt="iphone-right" class="iphone-right-img-right">					
+			</div>
+
+			<div class="row third-column">
+				<h2>Smart Click Optimization</h2>
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12">
+
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12">
+
+					</div>
 				</div>
 			</div>
 		</div>
